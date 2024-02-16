653de21aabd8240e4f6a77ee3e67c9904a1cfb32
LaterNerds <theo.ls.raymond@icloud.com>
2024-02-16 09:02:51 -0500

    Added tokenizer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,46 @@
-enum TYPE {
+#[derive(Debug)]
+pub enum TYPE {
     DOT,
     COMMA,
-    SPACE
+    SPACE,
+
+    NONE
 }
 
-struct token {
-    kind:TYPE,
-    tokenV: &str
+pub struct TOKEN {
+    pub kind:TYPE,
+    pub value: String
+}
+
+fn get_char(string:String, i: usize) -> char {
+    return string.as_bytes()[i] as char;
+}
+
+pub fn tokenize(code: String) -> Vec<TOKEN> {
+    let mut counter: usize = 0;
+    let mut tokens: Vec<TOKEN> = Vec::new();
+    while counter < code.len() {
+        println!("{}", counter);
+        let c: char = get_char(code.clone(), counter);
+        match c {
+            '.' => tokens.push(TOKEN {
+                kind:TYPE::DOT,
+                value: c.to_string()
+            }),
+            ',' => tokens.push(TOKEN {
+                kind:TYPE::COMMA,
+                value: c.to_string()
+            }),
+            ' ' => tokens.push(TOKEN {
+                kind:TYPE::SPACE,
+                value: c.to_string()
+            }),
+            _ => tokens.push(TOKEN {
+                kind: TYPE::NONE,
+                value: c.to_string()
+            })
+        }
+        counter += 1;
+    }
+    return tokens;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+mod lexer;
 
 fn main() -> std::io::Result<()> {
     let file = File::open("src/main.msq")?;
@@ -8,6 +9,19 @@ fn main() -> std::io::Result<()> {
     for line in reader.lines() {
         string.push_str(&line?);
     }
-    println!("{}", string);
+    println!("{}\n\n", string);
+    
+    println!("{}", lexer::tokenize(string).len());
     Ok(())
+}
+
+fn print_struct(token: lexer::TOKEN) {
+    print!("TYPE: ");
+    match token.kind {
+        lexer::TYPE::DOT => println!("DOT"),
+        lexer::TYPE::COMMA => println!("COMMA"),
+        lexer::TYPE::SPACE => println!("SPACE"),
+        lexer::TYPE::NONE => println!("NONE")
+    };
+    println!("VALUE: {}", token.value)
 }
