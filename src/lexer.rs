a00549383a3ef9c9c0defbeebac5c104213c7906
LaterNerds <theo.ls.raymond@icloud.com>
2024-02-18 18:35:28 -0500

    Finished lexer logic one memory leak to b fixed

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,46 +1,280 @@
 #[derive(Debug)]
 pub enum TYPE {
-    DOT,
-    COMMA,
-    SPACE,
+    // Operations
+    Plus,
+    PlusEquals,
+    Minus,
+    MinusEquals,
+    Divide,
+    DivideEquals,
+    Multiply,
+    MultiplyEquals,
+    Mod,
+    Bigger,
+    BiggerEquals,
+    Smaller,
+    SmallerEquals,
+    EqualTo,
 
-    NONE
+    // Single Character
+    Dot,
+    Comma,
+    Space,
+    Equal,
+    LBracket,
+    RBracket,
+    LBrace,
+    RBrace,
+    LParenthesis,
+    RParenthesis,
+    Colon,
+    SemiColon,
+
+    // Type Declaration
+    StringType,
+    IntType,
+    FloatType,
+    VarDec,
+
+    //Varrying values
+    Identifier
 }
 
 pub struct TOKEN {
-    pub kind:TYPE,
-    pub value: String
+    pub kind: TYPE,
+    pub value: String,
 }
 
-fn get_char(string:String, i: usize) -> char {
-    return string.as_bytes()[i] as char;
+/// Retrieves the character at the specified index in the given string.
+fn get_char(string: &str, i: usize) -> char {
+    string.as_bytes()[i] as char
 }
 
-pub fn tokenize(code: String) -> Vec<TOKEN> {
+/// Extracts a substring from the given string, starting from the 'start' index to the 'end' index.
+fn get_str(string: &str, start: usize, end: usize) -> String {
+    string[start..=end].to_string()
+}
+
+/// Tokenizes the input code string into a vector of TOKENs based on predefined character mappings.
+pub fn tokenize(code: &str) -> Vec<TOKEN> {
+    let mut value: String = String::new(); 
     let mut counter: usize = 0;
     let mut tokens: Vec<TOKEN> = Vec::new();
+    let mut id_counter: usize = 0;
+    // Iterate over the characters of the code string
     while counter < code.len() {
-        println!("{}", counter);
-        let c: char = get_char(code.clone(), counter);
+        let c: char = get_char(code, counter);
+
         match c {
+            '+' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "+= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::PlusEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Plus,
+                        value: c.to_string(),
+                    });
+                }
+            },
+            '-' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "-= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::MinusEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Minus,
+                        value: c.to_string(),
+                    });
+                }
+            },
+            '/' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "/= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::DivideEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Divide,
+                        value: c.to_string(),
+                    });
+                }
+            },
+            '*' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "*= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::MultiplyEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Multiply,
+                        value: c.to_string(),
+                    });
+                }
+            },
+            '>' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == ">= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::BiggerEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Bigger,
+                        value: c.to_string(),
+                    });
+                }
+            },
+            '<' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "<= " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::SmallerEquals,
+                        value: next_str,
+                    });
+                    counter += 1;
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Smaller,
+                        value: c.to_string(),
+                    });
+                }
+            },
             '.' => tokens.push(TOKEN {
-                kind:TYPE::DOT,
-                value: c.to_string()
+                kind: TYPE::Dot,
+                value: c.to_string(),
             }),
             ',' => tokens.push(TOKEN {
-                kind:TYPE::COMMA,
-                value: c.to_string()
+                kind: TYPE::Comma,
+                value: c.to_string(),
             }),
             ' ' => tokens.push(TOKEN {
-                kind:TYPE::SPACE,
-                value: c.to_string()
+                kind: TYPE::Space,
+                value: c.to_string(),
             }),
-            _ => tokens.push(TOKEN {
-                kind: TYPE::NONE,
-                value: c.to_string()
-            })
+            's' => {
+                let next_str = get_str(code, counter, counter + 2);
+                if next_str == "str " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::StringType,
+                        value: next_str,
+                    });
+                    counter += 2;
+                }
+            },
+            '%' => tokens.push(TOKEN {
+                kind: TYPE::Mod,
+                value: c.to_string(),
+            }),
+            '=' => {
+                let next_str = get_str(code, counter, counter + 1);
+                if next_str == "== " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::EqualTo,
+                        value: next_str,
+                    });
+                } else {
+                    tokens.push(TOKEN {
+                        kind: TYPE::Equal,
+                        value: "=".to_string(),
+                    });
+                }
+            },
+            '[' => tokens.push(TOKEN {
+                kind: TYPE::LBracket,
+                value: c.to_string(),
+            }),
+            ']' => tokens.push(TOKEN {
+                kind: TYPE::RBracket,
+                value: c.to_string(),
+            }),
+            '{' => tokens.push(TOKEN {
+                kind: TYPE::LBrace,
+                value: c.to_string(),
+            }),
+            '}' => tokens.push(TOKEN {
+                kind: TYPE::RBrace,
+                value: c.to_string(),
+            }),
+            '(' => tokens.push(TOKEN {
+                kind: TYPE::LParenthesis,
+                value: c.to_string(),
+            }),
+            ')' => tokens.push(TOKEN {
+                kind: TYPE::RParenthesis,
+                value: c.to_string(),
+            }),
+            ':' => tokens.push(TOKEN {
+                kind: TYPE::Colon,
+                value: c.to_string(),
+            }),
+            ';' => tokens.push(TOKEN {
+                kind: TYPE::SemiColon,
+                value: c.to_string(),
+            }),
+            'i' => {
+                let next_str = get_str(code, counter, counter + 2);
+                if next_str == "int " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::IntType,
+                        value: next_str,
+                    });
+                    counter += 2;
+                }
+            }
+            'f' => {
+                let next_str = get_str(code, counter, counter + 2);
+                if next_str == "flt " {
+                    tokens.push(TOKEN {
+                        kind: TYPE::FloatType,
+                        value: next_str,
+                    });
+                    counter += 2;
+                }
+            }
+            'h' => {
+                let next_str = get_str(code, counter, counter + 2);
+                if next_str == "hld" {
+                    tokens.push(TOKEN {
+                        kind: TYPE::VarDec,
+                        value: next_str,
+                    });
+                    counter += 2;
+                }
+            }
+            _ => {
+                if tokens[counter - 1].value == " " || tokens[counter - 1].value == "." || tokens[counter - 1].value == "," || tokens[counter - 1].value == "{" || tokens[counter - 1].value == "}" || tokens[counter - 1].value == "[" || tokens[counter - 1].value == "]" || tokens[counter - 1].value == "(" || tokens[counter - 1].value == ")" || tokens[counter - 1].value == ";" {
+                    while get_char(code, counter + id_counter) != '.' && get_char(code, counter + id_counter) != ',' && get_char(code, counter + id_counter) != '}' && get_char(code, counter + id_counter) != '{' && get_char(code, counter + id_counter) != '[' && get_char(code, counter + id_counter) != ']' && get_char(code, counter + id_counter) != '(' && get_char(code, counter + id_counter) != ')' && get_char(code, counter + id_counter) != ';' && get_char(code, counter + id_counter) != ':' && get_char(code, counter + id_counter) != '/' {
+                        value.push(get_char(code, counter + id_counter));
+                        id_counter += 1;
+                    }
+                    tokens.push(TOKEN {
+                        kind: TYPE::Identifier,
+                        value: value.clone(),
+                    });
+                    counter += id_counter;
+                    id_counter = 0;
+                }
+                
+            }
         }
         counter += 1;
     }
-    return tokens;
+    return tokens
 }
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,58 @@ use std::io::{BufRead, BufReader};
 mod lexer;
 
 fn main() -> std::io::Result<()> {
+    // Open the file specified (assuming "src/main.msq") and create a buffered reader.
     let file = File::open("src/main.msq")?;
     let reader = BufReader::new(file);
+
+    // Read the lines from the file and concatenate them into a single string.
     let mut string = String::new();
     for line in reader.lines() {
         string.push_str(&line?);
     }
-    println!("{}\n\n", string);
-    
-    println!("{}", lexer::tokenize(string).len());
+
+    // Tokenize the string using the lexer module and print the resulting tokens.
+    print_struct(lexer::tokenize(&string));
     Ok(())
 }
 
-fn print_struct(token: lexer::TOKEN) {
-    print!("TYPE: ");
-    match token.kind {
-        lexer::TYPE::DOT => println!("DOT"),
-        lexer::TYPE::COMMA => println!("COMMA"),
-        lexer::TYPE::SPACE => println!("SPACE"),
-        lexer::TYPE::NONE => println!("NONE")
-    };
-    println!("VALUE: {}", token.value)
+/// Prints the information about each token in the vector.
+fn print_struct(tokens: Vec<lexer::TOKEN>) {
+    for token in tokens {
+        print!("TYPE: ");
+        match token.kind {
+            lexer::TYPE::Identifier => println!("IDENTIFIER"),
+            lexer::TYPE::Dot => println!("DOT"),
+            lexer::TYPE::Comma => println!("COMMA"),
+            lexer::TYPE::Space => println!("SPACE"),
+            lexer::TYPE::Plus => println!("PLUS"),
+            lexer::TYPE::PlusEquals => println!("PLUS_EQUALS"),
+            lexer::TYPE::Minus => println!("MINUS"),
+            lexer::TYPE::MinusEquals => println!("MINUS_EQUALS"),
+            lexer::TYPE::Divide => println!("DIVIDE"),
+            lexer::TYPE::DivideEquals => println!("DIVIDE_EQUALS"),
+            lexer::TYPE::Multiply => println!("MULTIPLY"),
+            lexer::TYPE::MultiplyEquals => println!("MULTIPLY_EQUALS"),
+            lexer::TYPE::Mod => println!("MOD"),
+            lexer::TYPE::Bigger => println!("BIGGER"),
+            lexer::TYPE::BiggerEquals => println!("BIGGER_EQUALS"),
+            lexer::TYPE::Smaller => println!("SMALLER"),
+            lexer::TYPE::SmallerEquals => println!("SMALLER_EQUALS"),
+            lexer::TYPE::Equal => println!("EQUAL"),
+            lexer::TYPE::LBracket => println!("LBRACKET"),
+            lexer::TYPE::RBracket => println!("RBRACKET"),
+            lexer::TYPE::LBrace => println!("LBRACE"),
+            lexer::TYPE::RBrace => println!("RBRACE"),
+            lexer::TYPE::LParenthesis => println!("LPARENTHESIS"),
+            lexer::TYPE::RParenthesis => println!("RPARENTHESIS"),
+            lexer::TYPE::StringType => println!("STRING_TYPE"),
+            lexer::TYPE::IntType => println!("INT_TYPE"),
+            lexer::TYPE::FloatType => println!("FLOAT_TYPE"),
+            lexer::TYPE::VarDec => println!("VAR_DEC"),
+            lexer::TYPE::EqualTo => println!("EQUAL_TO"),
+            lexer::TYPE::Colon => println!("COLON"),
+            lexer::TYPE::SemiColon => println!("SEMICOLON")
+        };
+        println!("VALUE: \"{}\"", token.value);
+    }
 }
